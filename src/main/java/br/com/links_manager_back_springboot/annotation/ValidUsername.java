6a4cfa02ba5b50b095fe271b62553717158873b3
leandroltfx <leandroltfbr@hotmail.com>
2026-04-21 11:28:56 -0300

    Cria endpoint para cadastro de usuário

---
+++ src/main/java/br/com/links_manager_back_springboot/annotation/ValidUsername.java
@@ -0,0 +1,20 @@
+package br.com.links_manager_back_springboot.annotation;
+
+import br.com.links_manager_back_springboot.validator.UsernameValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = UsernameValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidUsername {
+
+    String message() default "Nome de usuário inválido.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
