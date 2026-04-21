c74e6e5f02d550f1d43693cc08192ce799d95817
leandroltfx <leandroltfbr@hotmail.com>
2026-04-21 09:47:11 -0300

    Configura geração e validação de token e define rotas públicas e privadas

---
+++ src/main/java/br/com/links_manager_back_springboot/service/JWTService.java
@@ -0,0 +1,50 @@
+package br.com.links_manager_back_springboot.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+public class JWTService {
+
+    @Value("${jwt-token-secret}")
+    private String jwtTokenSecret;
+
+    @Value("${jwt-token-issuer}")
+    private String jwtTokenIssuer;
+
+    public String generateToken(UUID subject) {
+        Algorithm algorithm = Algorithm.HMAC256(jwtTokenSecret);
+        Instant expiresAt = Instant.now().plus(Duration.ofHours(2));
+
+        return JWT
+                .create()
+                .withIssuer(jwtTokenIssuer)
+                .withSubject(subject.toString())
+                .withExpiresAt(expiresAt)
+                .sign(algorithm);
+    }
+
+    public String validateToken(String token) {
+        token = token.replace("Bearer ", "");
+
+        Algorithm algorithm = Algorithm.HMAC256(jwtTokenSecret);
+
+        try {
+            return JWT
+                    .require(algorithm)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException ex) {
+            return "";
+        }
+    }
+
+}
